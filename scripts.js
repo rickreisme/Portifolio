14b0901f04f658d67385caa491a333c35948fa9a
Rick <ricleyreis@gmail.com>
2023-05-19 19:10:49 -0300

    alteracao e readme

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var a = window.document.getElementById("header1")
+a.addEventListener('mouseenter', entrar)
+a.addEventListener('mouseout', sair)
+
+function entrar(){
+    var a = window.document.getElementById("header1")
+    
+}
+function sair(){
+    var a = window.document.getElementById("header1")
+    
+}
